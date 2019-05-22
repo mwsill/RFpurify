b28970b04815f2a0ef9f11daac9f00adc2d61b6b
mwsill <m.sill@dkfz.de>
2019-05-22 13:21:51 +0200

    add ESTIMATE purity

--- R/predict_purity.R
+++ R/predict_purity.R
@@ -3,11 +3,13 @@
 #' \code{predict_purity} predicts tumor purity using a minfi Mset object.
 #'
 #' @param Mset an object of class Mset
+#' @param method gold standard purity method for which purity is predicted, either 'ABSOLUTE' or 'ESTIMATE'  
 #' @export
 #' @import minfi
 
-predict_purity <- function(Mset) {
+predict_purity <- function(Mset,method=c("ABSOLUTE","ESTIMATE")) {
+  method <- match.arg(method)
   if(class(Mset) != 'MethylSet') stop('Mset is not of class MethylSet, try predict.mnp.betas for beta values')
   betas <- getBeta(Mset, type="Illumina")
-  predict_purity_betas(betas)
+  predict_purity_betas(betas,method)
 }
--- R/predict_purity_betas.R
+++ R/predict_purity_betas.R
@@ -3,10 +3,19 @@
 #' \code{predict_purity_betas} predicts tumor purity using a matrix of beta methylation values.
 #'
 #' @param betas a matrix of beta methylation values
+#' @param method gold standard purity method for which purity is predicted, either 'ABSOLUTE' or 'ESTIMATE'  
 #' @export
 #' @import randomForest
 
-predict_purity_betas <- function(betas) {
-  betas <- betas[match(rownames(puremeth$importance), rownames(betas)), , drop = FALSE]
-  predict(puremeth, t(betas))
+predict_purity_betas <- function(betas,method=c("ABSOLUTE","ESTIMATE")) {
+  method <- match.arg(method)
+  switch(method,
+         ABSOLUTE={
+           betas <- betas[match(rownames(RFpurify_ABSOLUTE$importance), rownames(betas)), , drop = FALSE]
+           predict(RFpurify_ABSOLUTE, t(betas))
+           },
+         ESTIMATE={
+           betas <- betas[match(rownames(RFpurify_ESTIMATE$importance), rownames(betas)), , drop = FALSE]
+           predict(RFpurify_ESTIMATE, t(betas))
+         })
 }
